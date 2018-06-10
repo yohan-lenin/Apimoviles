6d0cfec18a0c393128c335e4cb18bda342800fd3
yohan-lenin <yohan-lenin@hotmail.com>
2018-06-09 23:50:43 -0500

    Update connect.php

--- Api/app/libs/connect.php
+++ Api/app/libs/connect.php
@@ -4,8 +4,8 @@ if(!defined("SPECIALCONSTANT")) die("Acceso denegado");
 function getConnection(){
 	try {
 		$db_username = "root";
-		$db_password = "";
-		$connection = new PDO("mysql:host=localhost; dbname=neweco", $db_username, $db_password);
+		$db_password = "DXGqgh56840";
+		$connection = new PDO("mysql:host=172.18.3.251; dbname=NewEco", $db_username, $db_password);
 		$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		
 	} catch (PDOException $e) {
